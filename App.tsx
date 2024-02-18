200498def43bd68c26654b51ee06480b72c5ba7d
silpasheela <silpasheelassk@gmail.com>
2024-02-18 19:56:21 +0530

    added functionality to create password

--- App.tsx
+++ App.tsx
@@ -1,25 +1,44 @@
-
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
 
 import * as Yup from 'yup';
 
 const PasswordSchema = Yup.object().shape({
   passwordLength: Yup.number()
-  .min(4,'Minimum length should be 4 characters')
-  .max(16,'Maximum length should 16 characters')
-  .required('Please fill out this field')
-})
-
+    .min(4, 'Minimum length should be 4 characters')
+    .max(16, 'Maximum length should 16 characters')
+    .required('Please fill out this field'),
+});
 
 const App = () => {
+  const [password, setPassword] = useState('');
+  const [isPasswordGenerated, setIsPasswordGenerated] = useState(false);
+
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLength: number) => {};
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
+
+  const resetPassword = () => {};
+
   return (
     <View>
       <Text>App</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
